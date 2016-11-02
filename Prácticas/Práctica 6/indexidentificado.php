5b1c25d546fc3af573fdfda27ee907d92ffc118d
AdrianFL <adrianfranceslillo@gmail.com>
2016-11-02 20:45:21 +0100

    Acabada practica 6

---
+++ Prácticas/Práctica 6/indexidentificado.php
@@ -0,0 +1,7 @@
+<?php 
+$title="Página principal - Pictures & Images";
+require_once("includes/head.inc.php");
+require_once("includes/headeridentificado.inc.php");
+require_once("includes/principal.inc.php");
+require_once("includes/footer.inc.php");
+?>
